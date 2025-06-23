4749dfcfd77e8987bebb6fc3683d177c3d51afcb
Bogdan Lev <dartpab@gmail.com>
2025-06-23 14:18:37 +0300

    feat(DART-06): add task for conditional statements with if/else

--- 1_task.dart
+++ 1_task.dart
@@ -51,7 +51,7 @@ void main() {
   print('Can make purchase? = $canMakePurchase');
   print('Need to wait? = $needToWait');
 
-  //   Завдання 4: Тернарний оператор
+  // Завдання 4: Тернарний оператор
   // 1) Створіть змінну temperature.
   // 2) Використайте тернарний оператор, щоб визначити:
   // Якщо більше 25 — “Тепло”.
@@ -88,4 +88,31 @@ void main() {
   score *= 2;
   score ~/= totalQuestions;
   print('Final score: $score');
+
+  // Завдання 6: Умовні конструкції if/else
+  // 1) Створіть змінну examScore з значенням 56
+  // 2) Створіть умовні конструкції if / else if / else
+  // 3) Якщо більше 90 — вивести “Відмінно”.
+  // 4) Якщо 75–90 — “Добре”.
+  // 5) Якщо 60–74 — “Задовільно”.
+  // 6) Якщо менше 60 — “Не здано”.
+  // 7) Додатково: якщо оцінка менше 20 — вивести “Повторити курс”.
+  // 8) Поекспериментуйте, змінюючи значення examScore
+
+  int examScore = 56;
+
+  if (examScore > 90) {
+    print('Excellent');
+  } else if (examScore >= 75 && examScore <= 90) {
+    print('Good');
+  } else if (examScore >= 60 && examScore < 75) {
+    print('Satisfactory');
+  } else if (examScore < 60) {
+    print('Not passed');
+    if (examScore < 20) {
+      print('Repeat the course');
+    }
+  } else {
+    print('Invalid score');
+  }
 }
