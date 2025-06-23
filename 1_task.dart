4fe79db091edf7ccbb6855eb6d81801bfff786c7
Bogdan Lev <dartpab@gmail.com>
2025-06-23 13:56:46 +0300

    feat(DART-06): add task for assignment operators

--- 1_task.dart
+++ 1_task.dart
@@ -66,4 +66,26 @@ void main() {
   if (coldWeather.isNotEmpty) {
     print(coldWeather);
   }
+  // Завдання 5: Оператори присвоєння
+  // Використовувати тільки оператори присвоєння (+=, -=, *=, /=)
+  // 1) Створіть змінну score = 0.
+  // 2) Створіть зміни з даними:
+  // correctAnswers = 17;
+  // mistakes = 3;
+  // totalQuestions = 20;
+  // 3) Додайте 10 балів за кожну правильну відповідь.
+  // 4) Відніміть 5 балів за кожну помилку.
+  // 5) Збільшіть загальну кількість балів у 2 рази.
+  // 6) Поділіть бали на кількість запитань.
+  // 7) Виведіть остаточний результат.
+
+  int score = 0;
+  int correctAnswers = 17;
+  int mistakes = 3;
+  int totalQuestions = 20;
+  score += correctAnswers * 10;
+  score -= mistakes * 5;
+  score *= 2;
+  score ~/= totalQuestions;
+  print('Final score: $score');
 }
